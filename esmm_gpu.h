76ebad135666d4db94b1f0102f6041bd9b66ba88
Randal Burns <randal@cs.jhu.edu>
2024-11-14 16:47:23 -0500

    move to gpu

--- esmm_gpu.h
+++ esmm_gpu.h
@@ -40,7 +40,6 @@ __global__ void esmm_tile (int rows, int inners, int columns,
             int row = rTileOff + rowoff;
             int inner = iTileOff + inneroff;
 
-            // Unrolled coloff loop with columns == number threads
             //C[row * columns + (cTileOff + threadIdx.z)] += A[row * inners + inner] * B[inner * columns + (cTileOff + threadIdx.z)];
             atomicAdd(&C[row * columns + (cTileOff + threadIdx.z)], A[row * inners + inner] * B[inner * columns + (cTileOff + threadIdx.z)]);
 
@@ -55,21 +54,53 @@ __global__ void esmm_naive(int rows, int inners, int columns, const float *A,
     int xidx = blockIdx.x * blockDim.x + threadIdx.x;
     int yidx = blockIdx.y * blockDim.y + threadIdx.y;
 
-    // along rows and colums of AB for each element in C
-    int i=0;
+    // along rows and colums of AB for each element in C 
     float tmp = 0.0;
-    for (; i < inners; ++i)
+    for (int i=0;; i < inners; ++i)
     {
-	  // sums that work as a test/placeholder
-          // Row of A
-          //tmp += A[xidx * columns + i]; 
-          // Col of B
-          // tmp += B[i * inners + yidx]; 
-	  // Row of A  Col of B Sum
-          // tmp += A[xidx * columns + i] + B[i * inners + yidx]; 
-
-	  // Multiply works on full grid
           tmp += A[xidx * columns + i] * B[i * inners + yidx]; 
     }
     C[xidx * columns + yidx] = tmp;
+}
+
+__global__ void esmm_sequential (int rows, int inners, int columns, const float *A,
+                           const float *B, float *C)
+{
+    // change iteration order to output sequentially
+    const int x = blockIdx.x * BLOCKSIZE + (threadIdx.x / BLOCKSIZE);
+    const int y = blockIdx.y * BLOCKSIZE + (threadIdx.x % BLOCKSIZE);
+
+    // along rows and row of AB for each element in C
+    float tmp = 0.0;
+    for (int i=0; i < inners; ++i)
+    {
+          tmp += A[xidx * columns + i] * B[i * inners + yidx]; 
+    }
+    C[xidx * columns + yidx] = tmp;
+}
+
+__global__ void esmm_tile_noatomic (int rows, int inners, int columns, 
+				int rTileSize, int iTileSize, 
+				const float *A, const float *B, float *C)
+{
+    int rTileOff = blockIdx.x * rTileSize;
+    int iTileOff = blockIdx.y * iTileSize;
+    int cTileOff = blockIdx.z * blockDim.z; 
+
+    for (int rowoff = 0; rowoff < rTileSize; rowoff++)
+    {
+        for (int inneroff = 0; inneroff < iTileSize; inneroff++)
+        {
+            int row = rTileOff + rowoff;
+            int inner = iTileOff + inneroff; 
+
+            int cindex = row * columns + (cTileOff + threadIdx.z);
+            int aindex = row * inners + inner;
+            int bindex = inner * columns + (cTileOff + threadIdx.z);
+
+            //C[row * columns + (cTileOff + threadIdx.z)] += A[row * inners + inner] * B[inner * columns + (cTileOff + threadIdx.z)];
+            atomicAdd(&C[row * columns + (cTileOff + threadIdx.z)], A[row * inners + inner] * B[inner * columns + (cTileOff + threadIdx.z)]);
+
+        }
+    }
 }
